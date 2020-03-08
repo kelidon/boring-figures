8620d1ae5f5a870d21be46d31fe66ebb624c6402
kelidon <v.shchors@gmail.com>
2020-03-09 02:14:36 +0300

    create tools panel

--- src/by/bsu/boring_figures/BoringFigures.java
+++ src/by/bsu/boring_figures/BoringFigures.java
@@ -1,7 +1,21 @@
 package by.bsu.boring_figures;
 
+import by.bsu.boring_panels.ToolsPanel;
+
+import javax.swing.*;
+import java.awt.*;
+
+
 public class BoringFigures {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        var frame = new JFrame();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        var toolsPanel = new ToolsPanel();
+        frame.setPreferredSize(new Dimension(600,600));
+        frame.pack();
+        frame.add(toolsPanel, BorderLayout.LINE_START);
+        frame.setVisible(true);
     }
 }
---
+++ src/by/bsu/boring_panels/ToolsPanel.java
@@ -0,0 +1,54 @@
+package by.bsu.boring_panels;
+
+import by.bsu.boring_components.ColorChooserButton;
+
+import javax.swing.*;
+import java.awt.*;
+
+
+public class ToolsPanel extends JPanel {
+
+    static final String[] figures = new String[]{
+            "Ray",
+            "Line",
+            "Line segment",
+            "Polyline",
+
+            "Circle",
+            "Ellipse",
+            "Parallelogram",
+            "Rectangle",
+            "Polygon",
+            "Rectangle",
+            "Polyline",
+            "Regular polygon",
+            "Rhombus"};
+
+    public ToolsPanel() {
+        this.setLayout(new GridLayout(6, 1));
+        this.setSize(50, 200);
+
+        Label label = new Label("Figures");
+        add(label);
+
+        JComboBox<String> figureList = new JComboBox<>(figures);
+        add(figureList);
+
+        label = new Label("Border color");
+        add(label);
+
+        ColorChooserButton colorChooser = new ColorChooserButton(Color.WHITE);
+        colorChooser.addColorChangedListener(newColor -> {
+        });
+        add(colorChooser);
+
+        label = new Label("Fill color");
+        add(label);
+
+        colorChooser = new ColorChooserButton(Color.WHITE);
+        colorChooser.addColorChangedListener(newColor -> {
+        });
+        add(colorChooser);
+    }
+
+}
