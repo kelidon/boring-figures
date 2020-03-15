b9ae45ad140c0bb487d910407a21aa5b03e5ed08
El Quarasico <captainhorsepower228@gmail.com>
2020-03-15 22:35:04 +0300

    improve draw listener

--- src/by/bsu/boring_figures/boring_panels/DrawPanel.java
+++ src/by/bsu/boring_figures/boring_panels/DrawPanel.java
@@ -11,11 +11,11 @@ import javax.swing.border.BevelBorder;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.function.Consumer;
 
 import static by.bsu.boring_figures.boring_panels.ToolsPanel.figuresBinding;
 import static by.bsu.boring_figures.boring_panels.ToolsPanel.verticesSpinner;
@@ -31,107 +31,112 @@ public class DrawPanel extends JPanel {
     private final int DRAW_FIGURE_MASK = CTRL_MASK | CLEAR_POINTS_MASK;
     private final int COMBINED_MASK = MOVE_MASK | ADD_POINT_MASK | CLEAR_POINTS_MASK | DRAW_FIGURE_MASK;
 
-    private final MouseListener moveFigureListener = new MouseAdapter() {
-        @Override
-        public void mousePressed(MouseEvent e) {
-            if (matchesOnly(e.getModifiers(), MOVE_MASK)) {
-                if (selected != null) {
-                    selected.move(new Point(e.getX(), e.getY()));
-                    repaint();
-                    System.out.println("moved " + selected.getClass().getSimpleName());
-                }
-                return;
-            }
-
-            Point p = new Point(e.getX(), e.getY());
-            setSelected(figures.stream()
-                    .filter(f -> f.contains(p))
-                    .findFirst()
-                    .orElse(null));
-        }
-
-    };
-
-    private final MouseListener drawFigureMouseListener = new MouseAdapter() {
-
-        private final List<Point> points = new LinkedList<>();
-
-        @Override
-        public void mousePressed(MouseEvent e) {
-
-            if (matchesOnly(e.getModifiers(), ADD_POINT_MASK) || matchesOnly(e.getModifiers(), DRAW_FIGURE_MASK)) {
-                Point selectedPoint = new Point(e.getX(), e.getY());
-                points.add(selectedPoint);
-                drawSelectedPoint(selectedPoint);
-
-                boolean needMorePoints = false;
-                if (matchesOnly(e.getModifiers(), DRAW_FIGURE_MASK)) {
-                    Class<?> clazzNew = figuresBinding.get(ToolsPanel.figuresComboBox.getSelectedItem());
-
-                    builder.setClazz(clazzNew);
-                    builder.setPoints(points);
-                    builder.setVerticesNumber((int) verticesSpinner.getValue());
-
-                    try {
-                        Figure f = builder.build();
-                        addFigure(f);
-                    } catch (PointsShortageException ex) {
-                        JOptionPane.showMessageDialog(DrawPanel.this, "Need more points");
-                        System.out.println("need more points");
-                        needMorePoints = true;
-                    } catch (Error | Exception ex) {
-                        JOptionPane.showMessageDialog(DrawPanel.this, "Too much points. Selection cleared.");
-                        System.out.println("can't bulid");
-                    } finally {
-                        if (!needMorePoints) {
-                            figurePoints.clear();
-                            points.clear();
-                            repaint();
-                        }
-                    }
-                }
-                return;
-            }
-
-            if (matchesOnly(e.getModifiers(), CLEAR_POINTS_MASK)) {
-                points.clear();
-                return;
-            }
-        }
-
-        private void drawSelectedPoint(Point point) {
-            figurePoints.add(selectedPointMarker(point));
-            repaint();
-        }
-
-        private Figure selectedPointMarker(Point point) {
-            int pointRadius = 3;
-            return new Circle(
-                    new Point(point.getX() - pointRadius, point.getY() - pointRadius),
-                    new Point(point.getX() + pointRadius, point.getY() + pointRadius)
-            );
-        }
-    };
-
     private LinkedList<Figure> figures;
     private Figure selected;
     private FiguresBuilder builder;
-    private List<Figure> figurePoints;
+
+    // FIXME: 3/15/20 are those callbacks or listeners?
+    private List<Consumer<Graphics2D>> repaintListeners;
 
     public DrawPanel() {
         super(true);
         this.figures = new LinkedList<>();
         this.builder = new FiguresBuilder();
-        this.figurePoints = new ArrayList<>();
+        this.repaintListeners = new LinkedList<>();
 
         setBorder(new BevelBorder(BevelBorder.RAISED));
 
-        addMouseListener(moveFigureListener);
-        addMouseListener(drawFigureMouseListener);
-    }
+        // move figure listener
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                if (matchesOnly(e.getModifiers(), MOVE_MASK)) {
+                    if (selected != null) {
+                        selected.move(new Point(e.getX(), e.getY()));
+                        repaint();
+                        System.out.println("moved " + selected.getClass().getSimpleName());
+                    }
+                    return;
+                }
 
-    private boolean matches(int modifiers, int mask) {
-        return (modifiers & mask) != 0;
+                Point p = new Point(e.getX(), e.getY());
+                setSelected(figures.stream()
+                        .filter(f -> f.contains(p))
+                        .findFirst()
+                        .orElse(null));
+            }
+
+        });
+
+        addMouseListener(new MouseAdapter() {
+
+            private final List<Point> points = new LinkedList<>();
+
+            {
+                addRepaintListener(g2d -> points.stream()
+                        .map(this::selectedPointMarker)
+                        .forEach(f -> f.draw(g2d)));
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+                if (matchesOnly(e.getModifiers(), ADD_POINT_MASK) || matchesOnly(e.getModifiers(), DRAW_FIGURE_MASK)) {
+                    Point selectedPoint = new Point(e.getX(), e.getY());
+                    points.add(selectedPoint);
+                    repaint();
+
+                    boolean needMorePoints = false;
+                    if (matchesOnly(e.getModifiers(), DRAW_FIGURE_MASK)) {
+                        Class<?> clazzNew = figuresBinding.get(ToolsPanel.figuresComboBox.getSelectedItem());
+
+                        builder.setClazz(clazzNew);
+                        builder.setPoints(points);
+                        builder.setVerticesNumber((int) verticesSpinner.getValue());
+
+                        try {
+                            Figure f = builder.build();
+                            addFigure(f);
+                        } catch (PointsShortageException ex) {
+                            JOptionPane.showMessageDialog(DrawPanel.this, "Need more points");
+                            System.out.println("need more points");
+                            needMorePoints = true;
+                        } catch (Error | Exception ex) {
+                            JOptionPane.showMessageDialog(DrawPanel.this, "Too much points. Selection cleared.");
+                            System.out.println("can't bulid");
+                        } finally {
+                            if (!needMorePoints) {
+                                points.clear();
+                                repaint();
+                            }
+                        }
+                    }
+                    return;
+                }
+
+                if (matchesOnly(e.getModifiers(), CLEAR_POINTS_MASK)) {
+                    points.clear();
+                    return;
+                }
+            }
+
+            private Figure selectedPointMarker(Point point) {
+                //TODO 3/15/20: make it colorful and (maybe) randomized
+                int pointRadius = 3;
+                return new Circle(
+                        new Point(point.getX() - pointRadius, point.getY() - pointRadius),
+                        new Point(point.getX() + pointRadius, point.getY() + pointRadius)
+                );
+            }
+        });
+
+        // draw figures from bottom to top
+        addRepaintListener(g2d -> {
+            List<Figure> reversed = new ArrayList<>(figures);
+            Collections.reverse(reversed);
+            reversed.forEach(drawable -> drawable.draw(g2d));
+        });
+
     }
 
     private boolean matchesOnly(int modifiers, int mask) {
@@ -140,7 +145,6 @@ public class DrawPanel extends JPanel {
 
     public void addFigure(Figure figure) {
         this.figures.addFirst(figure);
-        this.figurePoints.clear();
         setSelected(figure);
         repaint();
     }
@@ -160,15 +164,16 @@ public class DrawPanel extends JPanel {
         }
     }
 
+    public void addRepaintListener(Consumer<Graphics2D> l) {
+        this.repaintListeners.add(l);
+    }
+
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponents(g);
         Graphics2D g2d = (Graphics2D) g;
         g2d.setStroke(new BasicStroke(5));
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        List<Figure> reversed = new ArrayList<>(figures);
-        Collections.reverse(reversed);
-        reversed.forEach(drawable -> drawable.draw(g2d));
-        figurePoints.forEach(drawable -> drawable.draw(g2d));
+        repaintListeners.forEach(l -> l.accept(g2d));
     }
 }
