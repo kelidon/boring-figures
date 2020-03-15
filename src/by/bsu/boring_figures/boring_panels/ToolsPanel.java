3cf64c97c96090a51fd9c9b225522c2532da8641
kelidon <v.shchors@gmail.com>
2020-03-15 18:36:59 +0300

    edit tools: figures' binding map, jspinner for regular polygons

--- src/by/bsu/boring_figures/boring_panels/ToolsPanel.java
+++ src/by/bsu/boring_figures/boring_panels/ToolsPanel.java
@@ -1,29 +1,37 @@
 package by.bsu.boring_figures.boring_panels;
 
+import by.bsu.boring_figures.actually_figures.*;
+import by.bsu.boring_figures.actually_figures.Polygon;
+import by.bsu.boring_figures.actually_figures.Rectangle;
 import by.bsu.boring_figures.boring_components.ColorChooserButton;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class ToolsPanel extends JPanel {
+    static final Map<String, Class<?>> figuresBinding = new HashMap<>();
 
-    static final String[] figures = new String[]{
-            "Ray",
-            "Line",
-            "Line segment",
-            "Polyline",
-            "Circle",
-            "Ellipse",
-            "Parallelogram",
-            "Rectangle",
-            "Polygon",
-            "Regular polygon",
-            "Rhombus"};
+    static {
+        figuresBinding.put("Ray", Ray.class);
+        figuresBinding.put("Line", Line.class);
+        figuresBinding.put("Line segment", LineSegment.class);
+        figuresBinding.put("Polyline", Polyline.class);
+        figuresBinding.put("Circle", Circle.class);
+        figuresBinding.put("Ellipse", Ellipse.class);
+        figuresBinding.put("Parallelogram", Parallelogram.class);
+        figuresBinding.put("Rectangle", Rectangle.class);
+        figuresBinding.put("Polygon", Polygon.class);
+        figuresBinding.put("Regular polygon", RegularPolygon.class);
+        figuresBinding.put("Rhombus", Rhombus.class);
+    }
 
-    static public JComboBox<String> figureList;
+    static public JComboBox<String> figuresComboBox;
     static public ColorChooserButton borderColorChooser;
     static public ColorChooserButton fillColorChooser;
+    static public JSpinner verticesSpinner;
 
     public ToolsPanel() {
         this.setLayout(new GridLayout(12, 1));
@@ -32,15 +40,14 @@ public class ToolsPanel extends JPanel {
         Label label = new Label("Figures");
         add(label);
 
-        figureList = new JComboBox<>(figures);
-        add(figureList);
+        figuresComboBox = new JComboBox<>();
+        figuresBinding.forEach((e1, e2) -> figuresComboBox.addItem(e1));
+        add(figuresComboBox);
 
         label = new Label("Border color");
         add(label);
 
         borderColorChooser = new ColorChooserButton(Color.WHITE);
-        borderColorChooser.addColorChangedListener(newColor -> {
-        });
         add(borderColorChooser);
 
         label = new Label("Fill color");
@@ -50,6 +57,13 @@ public class ToolsPanel extends JPanel {
         fillColorChooser.addColorChangedListener(newColor -> {
         });
         add(fillColorChooser);
+
+        label = new Label("Vertices number for regular polygon");
+        add(label);
+
+        verticesSpinner = new JSpinner();
+        verticesSpinner.setModel(new SpinnerNumberModel(3, 3, Integer.MAX_VALUE, 1));
+        add(verticesSpinner);
 
         repaint();
     }
